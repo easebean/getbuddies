46def0cd23ee8d400141c782a881ec1949f91126
iankityadav <1002ankit@gmail.com>
2021-05-15 21:21:23 +0530

    updated imports

--- src/main/java/com/getbuddies/app/repo/RoomRepo.java
+++ src/main/java/com/getbuddies/app/repo/RoomRepo.java
@@ -6,7 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.getbuddies.app.model.Room;
-import com.getbuddies.app.model.User;
 
 @Repository
 public interface RoomRepo extends JpaRepository<Room, Long> {
