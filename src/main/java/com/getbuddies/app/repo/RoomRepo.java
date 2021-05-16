075ea6ccdd2b49bed41793313e62938e71c5dfd4
iankityadav <1002ankit@gmail.com>
2021-05-16 07:39:25 +0530

    Added method to find rooms by name

--- src/main/java/com/getbuddies/app/repo/RoomRepo.java
+++ src/main/java/com/getbuddies/app/repo/RoomRepo.java
@@ -1,5 +1,6 @@
 package com.getbuddies.app.repo;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +11,6 @@ import com.getbuddies.app.model.Room;
 @Repository
 public interface RoomRepo extends JpaRepository<Room, Long> {
 	void deleteRoomById(Long id);
-
+	List<Room> findByNameContainingIgnoreCase(String name);
 	Optional<Room> findRoomById(Long id);
 }
