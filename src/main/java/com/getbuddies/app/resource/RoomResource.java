417c06acd8b5c4f0e9610f75daae4bec95e4329d
iankityadav <1002ankit@gmail.com>
2021-05-26 19:23:09 +0530

    Updated Room fields, methods and services

--- src/main/java/com/getbuddies/app/resource/RoomResource.java
+++ src/main/java/com/getbuddies/app/resource/RoomResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,9 +14,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.getbuddies.app.exception.RoomNotFoundException;
+import com.getbuddies.app.exception.UserNotFoundException;
 import com.getbuddies.app.model.Room;
 import com.getbuddies.app.service.RoomService;
 
+@CrossOrigin(origins = "http://localhost:4200",maxAge = 3600)
 @RestController
 @RequestMapping("/room")
 public class RoomResource {
@@ -31,6 +35,12 @@ public class RoomResource {
 		return new ResponseEntity<List<Room>>(rooms,HttpStatus.OK);
 	}
 	
+	@GetMapping("/{id}")
+	public ResponseEntity<Room> getRoomById(@PathVariable("id") Long id) throws RoomNotFoundException {
+		Room room = roomService.findRoomById(id);
+		return new ResponseEntity<Room>(room,HttpStatus.OK);
+	}
+	
 	@PostMapping("/create/{id}")
 	public ResponseEntity<Room> addRoom(@RequestBody Room room,@PathVariable("id") Long id){
 		Room newRoom = roomService.create(room,id);
@@ -43,15 +53,15 @@ public class RoomResource {
 		return new ResponseEntity<Room>(updateRoom,HttpStatus.OK);
 	}
 	
-	@PutMapping("/{roomId}/add/{id}")
-	public ResponseEntity<Room> addUser(@PathVariable("roomId") Long roomId,@PathVariable("id") Long id){
-		Room updateRoom = roomService.addUserToRoom(roomId,id);
+	@PutMapping("/add/{userName}")
+	public ResponseEntity<Room> addUser(@RequestBody Room room,@PathVariable("userName") String userName) throws UserNotFoundException{
+		Room updateRoom = roomService.addUserToRoom(room,userName);
 		return new ResponseEntity<Room>(updateRoom,HttpStatus.OK);
 	}
 	
-	@PutMapping("/{roomId}/remove/{id}")
-	public ResponseEntity<Room> removeUser(@PathVariable("roomId") Long roomId,@PathVariable("id") Long id){
-		Room updateRoom = roomService.removeUserFromRoom(roomId,id);
+	@PutMapping("/remove/{id}")
+	public ResponseEntity<Room> removeUser(@RequestBody Room room,@PathVariable("id") Long id){
+		Room updateRoom = roomService.removeUserFromRoom(room,id);
 		return new ResponseEntity<Room>(updateRoom,HttpStatus.OK);
 	}
 	
